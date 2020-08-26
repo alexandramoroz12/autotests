e5842616a35a42bbd6817c49c49475d65bd51ab5
alexandramoroz12 <alexandramoroz12@gmail.com>
2020-08-26 15:23:15 +0300

    Homework 5 - Wait. Part 3. Hope the last one

--- src/test/java/hw_5_Wait/WaitTest.java
+++ src/test/java/hw_5_Wait/WaitTest.java
@@ -1,13 +1,14 @@
 package test.java.hw_5_Wait;
 
-import com.sun.tools.internal.xjc.reader.xmlschema.BindYellow;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -15,10 +16,11 @@ import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertEquals;
 
+
 public class WaitTest {
 
     WebDriver driver;
-
+    WebDriverWait wait;
 
     /*
     By enterToAccountBy = By.xpath("//*[@class='header-topline__user-link link-dashed']");
@@ -42,17 +44,11 @@ public class WaitTest {
 
 
    // By enterToAccountBy2 = By.xpath("//*[@class='header-user-link sprite-side whitelink xhr']");
-    //By registerButtonOnEnterScreenBy2 = By.xpath("//*[@class='novisited auth-f-signup-link']");
+    //  By registerButtonOnEnterScreenBy2 = By.xpath("//*[@class='novisited auth-f-signup-link']");
    // By nameFieldBy2 = By.xpath("//*[@class='auth-f-i']//input[@name='title']");
    // By emailPhoneFieldBy2 = By.xpath("//*[@class='auth-f-i']//input[@name='login']");
    // By passwordFieldBy2 = By.xpath ("//*[@class='auth-f-i']//input[@name='password']");
    // By registerButtonOnSignUoScreenBy2 = By.xpath ("//*[@class='btn-link btn-link-green btn-link-sign']");
-
-
-    By errorNameFrame2 = By.xpath ("//input[@class='input-text auth-input-text input-invalid'][@name='title']");
-    By errorEmailFrame2 = By.xpath ("//input[@class='input-text auth-input-text input-invalid'][@name='login']");
-    By errorPasswordFrame2 = By.xpath ("//input[@class='input-text auth-input-text input-invalid'][@name='password']");
-
 
 
     By enterToAccountByMixed = By.xpath("//*[(@class='header-topline__user-link link-dashed') or (@class='header-user-link sprite-side whitelink xhr')]");
@@ -61,6 +57,12 @@ public class WaitTest {
     By emailPhoneFieldByMixed = By.xpath("//*[(@formcontrolname='username') or (@name='login')]");
     By passwordFieldByMixed = By.xpath("//*[(@formcontrolname='password') or (@name='password')]");
     By registerButtonOnSignUoScreenByMixed = By.xpath("//*[(@class='button button_size_large button_color_green auth-modal__submit') or (@class='btn-link btn-link-green btn-link-sign')]");
+
+
+    By errorNameFrame2 = By.xpath ("//input[@class='input-text auth-input-text input-invalid'][@name='title']");
+    By errorEmailFrame2 = By.xpath ("//input[@class='input-text auth-input-text input-invalid'][@name='login']");
+    By errorPasswordFrame2 = By.xpath ("//input[@class='input-text auth-input-text input-invalid'][@name='password']");
+
 
     @BeforeMethod
     public void setUp () {
@@ -72,30 +74,95 @@ public class WaitTest {
        // WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 10, 500);
 
     }
 
 
     @Test
-     public void test1 () {
+     public void test1AllFields () {
 
 
         driver.get("https://rozetka.com.ua/");
+
+
        // Actions actions = new Actions(driver);
         driver.findElement(enterToAccountByMixed).click();
         driver.findElement(registerButtonOnEnterScreenByMixed).click();
+
+        wait.until(ExpectedConditions.and(
+                ExpectedConditions.elementToBeClickable(nameFieldByMixed),
+                ExpectedConditions.elementToBeClickable(emailPhoneFieldByMixed),
+                ExpectedConditions.elementToBeClickable(passwordFieldByMixed)
+        ));
+
        // driver.findElement(surnameFieldBy).click();
         driver.findElement(nameFieldByMixed).click();
         driver.findElement(emailPhoneFieldByMixed).click();
         driver.findElement(passwordFieldByMixed).click();
         driver.findElement(registerButtonOnSignUoScreenByMixed).click();
 
-        String actualErrorMsg = driver.findElement(errorNameFrame2).toString();
-        String expectedErrorMsg = errorNameFrame2.toString();
-        assertEquals(actualErrorMsg, expectedErrorMsg);
+        WebElement nameInput = driver.findElement(errorNameFrame2);
+        String actualColorName = nameInput.getCssValue("border-color");
+        String expectedColor = "rgb(253, 124, 124)";
+        assertEquals(actualColorName, expectedColor);
 
+        WebElement emailInput = driver.findElement(errorEmailFrame2);
+        String actualColorEmail = emailInput.getCssValue("border-color");
+        assertEquals(actualColorEmail, expectedColor);
+
+
+        WebElement passInput = driver.findElement(errorPasswordFrame2);
+        String actualColorPass = passInput.getCssValue("border-color");
+        assertEquals(actualColorPass, expectedColor);
 
      }
+
+
+
+    @Test
+    public void test2TwoFields () {
+
+
+        driver.get("https://rozetka.com.ua/");
+
+
+
+
+        // Actions actions = new Actions(driver);
+        driver.findElement(enterToAccountByMixed).click();
+        driver.findElement(registerButtonOnEnterScreenByMixed).click();
+
+        wait.until(ExpectedConditions.and(
+                ExpectedConditions.elementToBeClickable(nameFieldByMixed),
+                ExpectedConditions.elementToBeClickable(emailPhoneFieldByMixed),
+                ExpectedConditions.elementToBeClickable(passwordFieldByMixed)
+        ));
+
+        // driver.findElement(surnameFieldBy).click();
+        driver.findElement(nameFieldByMixed).click();
+        //driver.findElement(emailPhoneFieldByMixed).click();
+        driver.findElement(passwordFieldByMixed).click();
+        driver.findElement(registerButtonOnSignUoScreenByMixed).click();
+
+        WebElement nameInput = driver.findElement(errorNameFrame2);
+        String actualColorName = nameInput.getCssValue("border-color");
+        String expectedColor = "rgb(253, 124, 124)";
+        assertEquals(actualColorName, expectedColor);
+
+        /*
+        WebElement emailInput = driver.findElement(errorEmailFrame2);
+        String actualColorEmail = emailInput.getCssValue("border-color");
+        assertEquals(actualColorEmail, expectedColor);
+        */
+
+        WebElement passInput = driver.findElement(errorPasswordFrame2);
+        String actualColorPass = passInput.getCssValue("border-color");
+        assertEquals(actualColorPass, expectedColor);
+
+    }
+
+
 
 
      @AfterMethod
